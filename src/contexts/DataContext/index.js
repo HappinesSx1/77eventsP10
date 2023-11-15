616b77ea4dd1f0a4512d6f4addb154a7c0428953
unknown <david.b57970@laposte.net>
2023-11-15 16:55:42 +0100

    init + img and slider fixes

---
+++ src/contexts/DataContext/index.js
@@ -0,0 +1,61 @@
+import PropTypes from "prop-types";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const json = await fetch("/events.json");
+    return json.json();
+  },
+};
+
+export const DataProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [lastEvent, setLastEvent] = useState(null);
+  const getData = useCallback(async () => {
+    try {
+      // setData(await api.loadData());
+      const apiData = await api.loadData();
+      setData(apiData);
+      const last = apiData.events.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+      )[0];
+      setLastEvent(last);
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+  useEffect(() => {
+    if (data) return;
+    getData();
+  });
+
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+        lastEvent,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useData = () => useContext(DataContext);
+
+export default DataContext;
